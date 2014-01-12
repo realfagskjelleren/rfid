5dfe02eb3511c1bad15145424dbd8dd304bd3b57
melwil <slettvold@gmail.com>
2014-01-12 20:39:12 +0100

    All methods need to throw exceptions so the POS knows something went wrong.

--- rfid/src/main/java/org/ntnu/realfagskjelleren/rfid/db/model/DBHandler.java
+++ rfid/src/main/java/org/ntnu/realfagskjelleren/rfid/db/model/DBHandler.java
@@ -11,18 +11,18 @@ public interface DBHandler {
     public boolean testConnection();
     public boolean createDatabase();
 
-    public User get_or_create(String rfid);
-    public User get_user(String ecc);
-    public void update_user_rfid(int userid, String rfid);
-    public boolean rfid_exists(String rfid);
-    public boolean ecc_exists(int ecc);
+    public User get_or_create(String rfid) throws SQLException;
+    public User get_user(int ecc) throws SQLException;
+    public void update_user_rfid(int userid, String rfid) throws SQLException;
+    public boolean rfid_exists(String rfid) throws SQLException;
+    public boolean ecc_exists(int ecc) throws SQLException;
 
-    public void deposit(String rfid, int value);
-    public void deduct(String rfid, int value);
+    public void deposit(String rfid, int value) throws SQLException;
+    public void deduct(String rfid, int value) throws SQLException;
 
-    public List<User> getAllUsers();
-    public List<Transaction> getTransactions(int... amount);
+    public List<User> getAllUsers() throws SQLException;
+    public List<Transaction> getTransactions(int... amount) throws SQLException;
 
-    public void transaction(int user_id, int value, boolean is_deposit, int new_balance);
-    public void log(String message);
+    public void transaction(int user_id, int value, boolean is_deposit, int new_balance) throws SQLException;
+    public void log(String message) throws SQLException;
 }
