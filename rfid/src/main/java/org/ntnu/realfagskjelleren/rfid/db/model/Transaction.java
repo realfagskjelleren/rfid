d20c859f1dfbca8fb632938a6dd2be65b5597164
melwil <slettvold@gmail.com>
2014-01-16 08:55:37 +0100

    Added some information to the Transaction objects.

--- rfid/src/main/java/org/ntnu/realfagskjelleren/rfid/db/model/Transaction.java
+++ rfid/src/main/java/org/ntnu/realfagskjelleren/rfid/db/model/Transaction.java
@@ -1,35 +1,49 @@
 package org.ntnu.realfagskjelleren.rfid.db.model;
 
+import java.sql.Timestamp;
+
 /**
  * @author Håvard Slettvold
  */
 public class Transaction {
 
-    private int userid;
+    private int user_id;
     private int value;
+    private int new_balance;
     private boolean is_deposit;
-    private String date;
+    private Timestamp date;
+    private String rfid;
 
-    public Transaction(int userid, int value, boolean is_deposit, String date) {
-        this.userid = userid;
+    public Transaction(int user_id, String rfid, int value, int new_balance, boolean is_deposit, Timestamp date) {
+        this.user_id = user_id;
+        this.rfid = rfid;
         this.value = value;
+        this.new_balance = new_balance;
         this.is_deposit = is_deposit;
         this.date = date;
     }
 
     public int getUserid() {
-        return userid;
+        return user_id;
+    }
+
+    public String getRfid() {
+        return rfid;
     }
 
     public int getValue() {
         return value;
     }
 
-    public boolean isIs_deposit() {
+    public int getNew_balance() {
+        return new_balance;
+    }
+
+    public boolean isDeposit() {
         return is_deposit;
     }
 
-    public String getDate() {
+    public Timestamp getDate() {
         return date;
     }
 
