93deb9e1d08d54ebc4c09ad09d7688ffb9e35161
melwil <slettvold@gmail.com>
2014-01-16 09:12:17 +0100

    Changed dbahndler to only take user id to filter transactions.

--- rfid/src/main/java/org/ntnu/realfagskjelleren/rfid/db/model/DBHandler.java
+++ rfid/src/main/java/org/ntnu/realfagskjelleren/rfid/db/model/DBHandler.java
@@ -13,7 +13,7 @@ public interface DBHandler {
 
     public User get_or_create(String rfid) throws SQLException;
     public User get_user(int ecc) throws SQLException;
-    public void update_user_rfid(int userid, String rfid) throws SQLException;
+    public void update_user_rfid(int user_id, String rfid) throws SQLException;
     public boolean rfid_exists(String rfid) throws SQLException;
     public boolean ecc_exists(int ecc) throws SQLException;
 
@@ -22,7 +22,7 @@ public interface DBHandler {
 
     public List<User> getAllUsers() throws SQLException;
     public List<Transaction> getTransactions(int amount) throws SQLException;
-    public List<Transaction> getTransactions(User user, int amount) throws SQLException;
+    public List<Transaction> getTransactions(int user_id, int amount) throws SQLException;
 
     public void transaction(int user_id, int value, boolean is_deposit, int new_balance) throws SQLException;
     public void log(String message) throws SQLException;
