a3e9c0c4955c7cd94aa9a32d2a3d61c4d1c2c085
melwil <slettvold@gmail.com>
2014-01-27 22:12:58 +0100

    Moved the 'migration' that handles sqlite -> mysql

--- rfid/src/main/java/org/ntnu/realfagskjelleren/rfid/db/model/migrations/ConvertDataFromRFID1.java
+++ rfid/src/main/java/org/ntnu/realfagskjelleren/rfid/db/model/migrations/ConvertDataFromRFID1.java
@@ -1,4 +1,4 @@
-package org.ntnu.realfagskjelleren.rfid;
+package org.ntnu.realfagskjelleren.rfid.db.model.migrations;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
